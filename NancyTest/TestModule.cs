432eb3e4e904f7ad4918b4af2abf5dabd666c21c
Will Munn <willmunn@hotmail.com>
2012-04-01 19:02:08 +0100

    added view rendering

--- NancyTest/TestModule.cs
+++ NancyTest/TestModule.cs
@@ -4,12 +4,22 @@ namespace NancyTest
 {
 	public class TestModule : Nancy.NancyModule
 	{
+		private Handler _myHandler;
+				
 		public TestModule ()
 		{
-			Get["/"] = _ => "Hello World";
+			_myHandler=new Handler();
+			Get["/"] = _ => _myHandler.Handle();
 			
 			Post["/"] = _ => "Thank you for your letter";
+			
+			Get["/view"] = _ => {return View["test"];};
+		}
+	}
+	
+	public class Handler{
+		public string Handle(){
+			return "Hello World";
 		}
 	}
 }
-
